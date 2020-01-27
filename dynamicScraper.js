7a0e1cfaa06262e23d9ddfd753011bfdd8b383c6
Sonia Rottenberg <sonia.rottenberg@gmail.com>
2020-01-27 18:38:05 +0100

    added pupeteer

--- dynamicScraper.js
+++ dynamicScraper.js
@@ -2,3 +2,17 @@ const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
 
 const url = "https://www.glassdoor.fr/Emploi/france-interessement-emplois-SRCH_IL.0,6_IN86_KO7,20.htm"
+
+puppeteer
+    .launch()
+    .then(browser => browser.newPage)
+    .then(page => {
+        return page.goto(url).then(function(){
+            return.page.content();
+        });
+    })
+    .then(html => {
+        const $ = cheerio.load(html);
+        const results = []
+        $('#Details').push
+    })
